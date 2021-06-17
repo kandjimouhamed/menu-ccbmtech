acacabd5a22f34c46fcee05195e0157aff2395b2
kandjimouhamed <kandjimouhamed@gmail.com>
2021-06-17 12:59:24 +0000

    first commit

---
+++ menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="menu.css">
+    <title>menu</title>
+</head>
+<body>
+    <header class ="topbar">
+        <a class="topbar-logo" href="#" ><img src="images/im_logo.jpg" alt=""></a>
+        <a href="#" class="nav-icons"></a>
+        <nav class="topbar-nav" >
+            <a class="first-child" href="#">A propos <br> <span class="span"> Qui Somme Nous?</span></a>
+            <a href="#">Savoir-faire <br> <span class="span"> Notre cœur de metier</span></a>
+            <a href="#">Nos Services <br><span class="span">Listing</span> </a>
+            <a href="#">Service à la clientèle <br> <span class="span"> Fidélisation</span></a>
+            <a href="#">Contacts <br> <span class="span"> Rejoignez-nous</span></a> 
+            
+        </nav>
+    </header>
+</body>
+</html>
